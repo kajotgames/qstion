f39bc15853aca33c2583d46294329b24c72b7ef5
Marek Nemeth <nemeth@gitlab.kajot.cz>
2024-08-29 10:18:09 +0200

    patch: formatting on stringifier, minor fix - fixed iteration over root's child nodes

--- src/qstion/stringifier.py
+++ src/qstion/stringifier.py
@@ -2,18 +2,27 @@ from .base import QS, QsNode
 import urllib.parse as up
 import typing as t
 
-LIST_FORMAT_OPTIONS = ['indices', 'brackets', 'repeat', 'comma']
+LIST_FORMAT_OPTIONS = ["indices", "brackets", "repeat", "comma"]
 
 
 class QsStringifier(QS):
-    _af: str = 'indices'
+    _af: str = "indices"
 
-    def __init__(self, depth: int = 5, parameter_limit: int = 1000, allow_dots: bool = False, allow_sparse: bool = False, array_limit: int = 20, parse_arrays: bool = False, allow_empty: bool = False, comma: bool = False, array_format: str = 'indices'):
-        super().__init__(depth, parameter_limit, allow_dots,
-                         array_limit, parse_arrays, allow_empty, comma)
+    def __init__(
+        self,
+        depth: int = 5,
+        parameter_limit: int = 1000,
+        allow_dots: bool = False,
+        allow_sparse: bool = False,
+        array_limit: int = 20,
+        parse_arrays: bool = False,
+        allow_empty: bool = False,
+        comma: bool = False,
+        array_format: str = "indices",
+    ):
+        super().__init__(depth, parameter_limit, allow_dots, array_limit, parse_arrays, allow_empty, comma)
         if array_format not in LIST_FORMAT_OPTIONS:
-            raise ValueError(
-                f'array_format must be one of {LIST_FORMAT_OPTIONS}')
+            raise ValueError(f"array_format must be one of {LIST_FORMAT_OPTIONS}")
         self._af = array_format
 
     def stringify(self, data: dict, filter: list = None) -> list[tuple]:
@@ -24,30 +33,30 @@ class QsStringifier(QS):
             data (dict): dictionary to process
             filter (list): list of keys to filter
 
-        Returns:    
+        Returns:
             str: query string
         """
         _q_args = {}
         for item in data:
-            node = QsNode.load(
-                item,
-                data[item],
-                filter=filter)
+            node = QsNode.load(item, data[item], filter=filter)
             if node is not None:
                 self._root_node[node.key] = node
-        for item in self._root_node.values():
+        for item in self._root_node:
             for arg in self._get_arg(item):
                 if arg is None:
                     continue
                 prepared_arg = self._transform_arg(arg)
                 if prepared_arg[0] in _q_args:
-                    _q_args[prepared_arg[0]] = [_q_args[prepared_arg[0]], prepared_arg[1]] if isinstance(
-                        _q_args[prepared_arg[0]], str) else [*_q_args[prepared_arg[0]], prepared_arg[1]]
+                    _q_args[prepared_arg[0]] = (
+                        [_q_args[prepared_arg[0]], prepared_arg[1]]
+                        if isinstance(_q_args[prepared_arg[0]], str)
+                        else [*_q_args[prepared_arg[0]], prepared_arg[1]]
+                    )
                 else:
                     _q_args[prepared_arg[0]] = prepared_arg[1]
         res = []
         for key, value in _q_args.items():
-            if isinstance(value, list) and (self._af == 'repeat' or self._af == 'brackets'):
+            if isinstance(value, list) and (self._af == "repeat" or self._af == "brackets"):
                 res += [(key, v) for v in value]
             else:
                 res.append((key, str(value)))
@@ -67,18 +76,18 @@ class QsStringifier(QS):
             # decide based on array_format
             k = key[0]
             k = key[0]
-            if isinstance(k, int) and (self._af == 'repeat' or self._af == 'brackets'):
-                return '[]' if self._af == 'brackets' else None
+            if isinstance(k, int) and (self._af == "repeat" or self._af == "brackets"):
+                return "[]" if self._af == "brackets" else None
             else:
-                return f'[{k}]' if not self._allow_dots else k
+                return f"[{k}]" if not self._allow_dots else k
         else:
             current_key = key.pop()
             formatted = self._format_key(key)
             if formatted is None:
-                return f'[{current_key}]' if not self._allow_dots else f'.{current_key}'
+                return f"[{current_key}]" if not self._allow_dots else f".{current_key}"
             if self._allow_dots:
-                return f'{current_key}.{formatted}'
-            return f'[{current_key}]{formatted}'
+                return f"{current_key}.{formatted}"
+            return f"[{current_key}]{formatted}"
 
     def _get_arg(self, node: QsNode) -> tuple[str, t.Any]:
         """
@@ -95,8 +104,8 @@ class QsStringifier(QS):
                 yield None
             else:
                 yield ([node.key], node.value)
-        elif node.is_default_array() and self._af == 'comma':
-            yield ([node.key], ','.join([str(child.value) for child in node.children]))
+        elif node.is_default_array() and self._af == "comma":
+            yield ([node.key], ",".join([str(child.value) for child in node.children]))
         else:
             for child in node.children:
                 for arg in self._get_arg(child):
@@ -125,17 +134,17 @@ class QsStringifier(QS):
 
 
 def stringify(
-        data: dict,
-        allow_dots: bool = False,
-        encode: bool = True,
-        delimiter: str = '&',
-        encode_values_only: bool = False,
-        array_format: str = 'indices',
-        sort: bool = False,
-        sort_reverse: bool = False,
-        charset: str = 'utf-8',
-        filter: list = None,
-        charset_sentinel: bool = False,
+    data: dict,
+    allow_dots: bool = False,
+    encode: bool = True,
+    delimiter: str = "&",
+    encode_values_only: bool = False,
+    array_format: str = "indices",
+    sort: bool = False,
+    sort_reverse: bool = False,
+    charset: str = "utf-8",
+    filter: list = None,
+    charset_sentinel: bool = False,
 ) -> str:
     """
     Process a dictionary into a query string
@@ -153,20 +162,17 @@ def stringify(
         filter (list): list of keys to filter
         charset_sentinel (bool): add utf8 sentinel
 
-    Returns:    
+    Returns:
         str: query string
     """
-    qs = QsStringifier(
-        allow_dots=allow_dots,
-        array_format=array_format
-    )
+    qs = QsStringifier(allow_dots=allow_dots, array_format=array_format)
     _qt = qs.stringify(data, filter=filter)
     if charset_sentinel:
-        _qt.insert(0, ('utf8', '✓'))
+        _qt.insert(0, ("utf8", "✓"))
     if sort:
         _qt = sorted(_qt, key=lambda x: x[0], reverse=sort_reverse)
     if encode:
         return delimiter.join([qs._q(k, v, charset=charset) for k, v in _qt])
     elif encode_values_only:
         return delimiter.join([f'{k}={up.quote(v, encoding=charset, errors="xmlcharrefreplace")}' for k, v in _qt])
-    return delimiter.join([f'{k}={v}' for k, v in _qt])
+    return delimiter.join([f"{k}={v}" for k, v in _qt])
