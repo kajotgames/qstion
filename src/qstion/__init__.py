af5da3ec9f1501296f98ed332cae1129e3fbb53d
Marek Nemeth <nemeth@gitlab.kajot.cz>
2024-09-04 12:02:17 +0200

    update version: __init__.py file

--- src/qstion/__init__.py
+++ src/qstion/__init__.py
@@ -1,7 +1,7 @@
 from .parser import parse, QsParser
 from .stringifier import stringify, QsStringifier
 
-VERSION = (1, 0, 2)
+VERSION = (1, 1, 0)
 __version__ = ".".join(map(str, VERSION))
 
 __all__ = ["parse", "QsParser", "stringify", "QsStringifier"]
