8f9c455d5c278fdfb4d3fda07167d4314f8f5b57
Marek Nemeth <nemeth@gitlab.kajot.cz>
2024-09-09 11:45:37 +0200

    chore: versioning, changelog

--- src/qstion/__init__.py
+++ src/qstion/__init__.py
@@ -1,7 +1,7 @@
 from .parser import parse, QsParser
 from .stringifier import stringify, QsStringifier
 
-VERSION = (1, 1, 0)
+VERSION = (1, 1, 1)
 __version__ = ".".join(map(str, VERSION))
 
 __all__ = ["parse", "QsParser", "stringify", "QsStringifier"]
